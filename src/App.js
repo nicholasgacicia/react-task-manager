cf0d3a9df8245c479c48bf8e19b62e2133ee1990
nicholasgacicia <nicholasgacicia@gmail.com>
2021-04-17 09:32:03 -0500

    Basic functionality complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { React, useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -24,6 +25,15 @@ const App = () => {
     }
   ])
 
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -40,7 +50,13 @@ const App = () => {
 
   return (
     <div className='container'>
-      <Header />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask
+        onAdd={addTask}
+      />}
       {tasks.length > 0 ? (
         <Tasks
           tasks={tasks}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
-import Button from "./Button";
+import Button from './Button';
 
-
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log(`Clicked!!!`);
-  }
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1>Task Tracker</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      <Button
+        color={showAdd ? 'red' : 'green'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />
     </header>
   )
 }
